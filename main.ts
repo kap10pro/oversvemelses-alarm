1e419b433d2d063e449a769d9050557be52dd432
kap10pro <128356999+kap10pro@users.noreply.github.com>
2023-03-26 13:33:50 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
+makerbit.connectUltrasonicDistanceSensor(DigitalPin.P2, DigitalPin.P3)
 basic.forever(function () {
-	
+    while (makerbit.isUltrasonicDistanceLessThan(7, DistanceUnit.CM)) {
+        music.playMelody("- - - - - - - - ", 120)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
 })
